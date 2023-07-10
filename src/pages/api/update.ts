c3a895bc101ab58913f9ce81edce7def31ad665c
sam <sublimecraftftw@gmail.com>
2023-07-09 19:23:41 -0700

    Temp disable build warning

--- src/pages/api/update.ts
+++ src/pages/api/update.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/require-await */
 import { verifySignature } from "@upstash/qstash/nextjs";
 import type { NextApiHandler } from "next";
 import { env } from "@/env.mjs";

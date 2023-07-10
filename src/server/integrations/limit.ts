5311eb04343e2d6042f7a695bb0063d393f1f595
sam <sublimecraftftw@gmail.com>
2023-07-09 19:52:30 -0700

    Rate limit fn

--- src/server/integrations/limit.ts
+++ src/server/integrations/limit.ts
@@ -1,1 +1,12 @@
 import { Ratelimit } from "@upstash/ratelimit";
+import { redis } from "./upstash";
+
+type Unit = "ms" | "s" | "m" | "h" | "d";
+type Duration = `${number} ${Unit}` | `${number}${Unit}`;
+
+export const limit = (requests: number, window: Duration) => {
+  return new Ratelimit({
+    redis,
+    limiter: Ratelimit.slidingWindow(requests, window),
+  });
+};
